b8b030e19715bc183154050e4114e84e7411f153
Diego Ribera <47877090+DRiberaC@users.noreply.github.com>
2022-02-07 06:11:15 -0400

    Primera revision
    primera revision de la pagina para el sistema contable eicap

---
+++ resources/views/onepage.blade.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Página de información del sistema contable eicap.">
+    <meta name="keywords" content="eicap,CCTP-EICAP,sistema contable eicap,escuela de capacitacion eicap">
+    <meta name="author" content="CCTP-EICAP">
+    <title>Sistema Contable EICAP</title>
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="assets/css/plugins.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="onepage" data-bs-spy="scroll" data-bs-target=".navbar">
+    <div class="content-wrapper">
+        <header class="wrapper bg-gray">
+            <nav class="navbar navbar-expand-lg center-logo transparent navbar-light">
+                <div class="container flex-lg-row flex-nowrap align-items-center">
+                    <div class="navbar-brand w-100">
+                        {{-- <a href="index-2.html">
+                            <img src="assets/img/logo.png" srcset="./assets/img/logo@2x.png 2x" alt="" />
+                        </a> --}}
+                    </div>
+                    <div class="navbar-collapse offcanvas-nav">
+                        <div class="offcanvas-header d-lg-none d-xl-none">
+                            {{-- <a href="index-2.html"><img src="assets/img/logo-light.png"
+                                    srcset="./assets/img/logo-light@2x.png 2x" alt="" /></a> --}}
+                            <button type="button" class="btn-close btn-close-white offcanvas-close offcanvas-nav-close"
+                                aria-label="Close"></button>
+                        </div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item"><a class="nav-link scroll" href="#home">Inicio</a></li>
+                            <li class="nav-item"><a class="nav-link scroll"
+                                    href="#caracteristicas">Características</a></li>
+                            <li class="nav-item"><a class="nav-link scroll" href="#presentacion">Presentación</a>
+                            </li>
+                        </ul>
+                        <!-- /.navbar-nav -->
+                    </div>
+                    <!-- /.navbar-collapse -->
+                    <div class="navbar-other w-100 d-flex ms-auto">
+                        <ul class="navbar-nav flex-row align-items-center ms-auto" data-sm-skip="true">
+                            <li class="nav-item d-none d-md-block">
+                                {{-- <a href="contact.html" class="btn btn-sm btn-primary rounded-pill">Descargar</a> --}}
+                                <a href="https://keys-verification.eicap.com.bo/public/storage/uploads/SistemaContableEICAPV3.3.1.exe"
+                                    id="get-request" class="btn btn-sm btn-primary rounded-pill" download>Descargar</a>
+                            </li>
+                            <li class="nav-item d-lg-none">
+                                <div class="navbar-hamburger"><button class="hamburger animate plain"
+                                        data-toggle="offcanvas-nav"><span></span></button></div>
+                            </li>
+                        </ul>
+                        <!-- /.navbar-nav -->
+                    </div>
+                    <!-- /.navbar-other -->
+                </div>
+                <!-- /.container -->
+            </nav>
+            <!-- /.navbar -->
+        </header>
+        <!-- /header -->
+        <section id="home">
+            <div class="wrapper bg-gray">
+                <div class="container pt-10 pt-md-14 pb-14 pb-md-17 text-center">
+                    <div class="row text-center">
+                        <div class="col-lg-9 col-xxl-7 mx-auto" data-cues="zoomIn" data-group="welcome"
+                            data-interval="-200">
+                            <h2 class="display-1 mb-4">Sistema Contable EICAP</h2>
+                            <p class="lead fs-24 lh-sm px-md-5 px-xl-15 px-xxl-10">Creado para mejorar de manera
+                                eficiente la información contable, económica y financiera de la empresa.</p>
+                        </div>
+                        <!-- /column -->
+                    </div>
+                    <!-- /.row -->
+                    <div class="row text-center mt-10">
+                        <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
+                            <figure><img class="w-auto" src="assets/img/capturas/CapturaAsientoContable.png"
+                                    srcset="./assets/img/illustrations/i8@2x.png 2x" alt="" /></figure>
+                        </div>
+                        <!-- /column -->
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.container -->
+            </div>
+            <!-- /.wrapper -->
+        </section>
+        <!-- /section -->
+        <section id="caracteristicas">
+            <div class="wrapper bg-light">
+                <div class="container py-4 py-md-4">
+                    <div class="row gx-lg-8 gx-xl-12 gy-6 mb-10 align-items-center">
+                        <div class="row gx-3 gy-10 align-items-center">
+                            <div class="col-lg-5 offset-lg-1">
+                                <figure><img class="w-auto" src="assets/img/illustrations/3d2.png"
+                                        srcset="./assets/img/illustrations/3d2@2x.png 2x" alt="" /></figure>
+                            </div>
+                            <!--/column -->
+                            <div class="col-lg-5 offset-lg-1">
+                                <h2 class="fs-16 text-uppercase text-gradient gradient-1 mb-3">Toma el control de tus
+                                    finanzas</h2>
+                                <h3 class="display-4 mb-4">Utiliza el mejor sistema contable de Bolivia.</h3>
+                                <p class="mb-6">Presente en los 9 departamentos de Bolivia y utilizado por
+                                    cientos de profesionales contables alrededor de toda Bolivia, interfaz amigable y
+                                    fácil instalación. Con actualizaciones periódicas acorde a las normativas
+                                    impositivas.</p>
+                                <div class="row gy-3">
+                                    <div class="col-xl-6">
+                                        <ul class="icon-list bullet-bg bullet-soft-primary mb-0">
+                                            <li><span><i class="uil uil-check"></i></span><span>Contabilidad
+                                                    automatizada.</span></li>
+                                            <li class="mt-3"><span><i
+                                                        class="uil uil-check"></i></span><span>Libro de Compras y
+                                                    ventas (Estándar, SIAT).</span></li>
+                                        </ul>
+                                    </div>
+                                    <!--/column -->
+                                    <div class="col-xl-6">
+                                        <ul class="icon-list bullet-bg bullet-soft-primary mb-0">
+                                            <li><span><i class="uil uil-check"></i></span><span>Multiempresa.</span>
+                                            </li>
+                                            <li class="mt-3"><span><i
+                                                        class="uil uil-check"></i></span><span>Reportes de E.E.F.F.
+                                                </span></li>
+                                        </ul>
+                                    </div>
+                                    <!--/column -->
+                                </div>
+                                <!--/.row -->
+                            </div>
+                            <!--/column -->
+                        </div>
+                    </div>
+                    <!--/.row -->
+                    <div class="row gx-lg-8 gx-xl-12 gy-6 gy-md-0 text-center">
+                        <div class="col-md-6 col-lg-3">
+                            <img src="assets/img/icons/lineal/analytics.svg"
+                                class="svg-inject icon-svg icon-svg-md text-blue mb-3" alt="" />
+                            <h4>Contabilidad automatizada</h4>
+                            <p class="mb-2">Podrás generar todos los asientos contables de forma automática
+                                desde LCV. Optimizando tu productividad.</p>
+                        </div>
+                        <!--/column -->
+                        <div class="col-md-6 col-lg-3">
+                            <img src="assets/img/icons/lineal/check-list.svg"
+                                class="svg-inject icon-svg icon-svg-md text-green mb-3" alt="" />
+                            <h4>Libro de Compras y ventas (Estándar, SIAT).</h4>
+                            <p class="mb-2">Importa todas tus facturas en formato <strong>Excel</strong> o
+                                <strong>.txt</strong> , también puedes importar desde el formato <strong>.csv</strong>
+                                desde el nuevo Sistema SIAT.
+                            </p>
+                        </div>
+                        <!--/column -->
+                        <div class="col-md-6 col-lg-3">
+                            <img src="assets/img/icons/lineal/settings-3.svg"
+                                class="svg-inject icon-svg icon-svg-md text-yellow mb-3" alt="" />
+                            <h4>Multiempresa</h4>
+                            <p class="mb-2">Administra todas tus empresas desde un solo lugar, simplificando
+                                el manejo de tus operaciones.</p>
+                        </div>
+                        <!--/column -->
+                        <div class="col-md-6 col-lg-3">
+                            <img src="assets/img/icons/lineal/bar-chart.svg"
+                                class="svg-inject icon-svg icon-svg-md text-orange mb-3" alt="" />
+                            <h4>Reportes de E.E.F.F.</h4>
+                            <p class="mb-2">Genera de forma automática todos los E.E.F.F., Balance General,
+                                Sumas y Saldos, Estado de Resultado, Flujo de Efectivo, Evolución de Patrimonio, Libro
+                                Diario y Libros Mayores.</p>
+                        </div>
+                        <!--/column -->
+                    </div>
+                    <!--/.row -->
+                </div>
+                <!-- /.container -->
+            </div>
+            <!-- /.wrapper -->
+        </section>
+        <!-- /section -->
+        <!-- /section -->
+        <section id="presentacion" class="wrapper bg-light">
+            <div class="container pt-15 pt-md-17">
+                <div class="row gy-10 gy-sm-13 gx-md-8 gx-xl-12 align-items-center mb-15 mb-md-17">
+                    <div class="col-lg-6 position-relative">
+                        {{-- <a href="https://content.jwplatform.com/videos/H1OYSCR0-c6G4Isae.mp4"
+                            class="btn btn-circle btn-white btn-play ripple mx-auto mb-5 position-absolute"
+                            style="top:50%; left: 50%; transform: translate(-50%,-50%); z-index:3;" data-glightbox><i
+                                class="icn-caret-right"></i></a>
+                        <figure class="rounded"><img src="assets/img/capturas/CapturaInicio.png"
+                                srcset="./assets/img/photos/about12@2x.jpg 2x" alt=""></figure> --}}
+                        <video poster="assets/img/capturas/CapturaInicio.png" class="player" playsinline
+                            controls preload="none">
+                            <source src="https://content.jwplatform.com/videos/H1OYSCR0-c6G4Isae.mp4" type="video/mp4">
+                            <!-- <source src="https://content.jwplatform.com/videos/H1OYSCR0-c6G4Isae.mp4" type="video/mp4"> -->
+                        </video>
+                    </div>
+                    <!--/column -->
+                    <div class="col-lg-6">
+                        <h3 class="display-3 mb-8">Sistema Contable EICAP</h3>
+                        <h2 class="fs-16 text-uppercase text-muted mb-3">Presentación del sistema contable EICAP</h2>
+                        <p class="mb-6">El sistema contable EICAP cuenta conta todas funciones necesarias para
+                            llevar a cabo una contabilidad profesional de tus empresas, elaborando todos los E.E.F.F. de
+                            forma automática.</p>
+                        <!--/.row -->
+                    </div>
+                    <!--/column -->
+                </div>
+                <!--/.row -->
+                <div class="row text-center mb-7">
+                    <div class="col-lg-11 col-xl-10 col-xxl-9 mx-auto">
+                        {{-- <h2 class="fs-16 text-uppercase text-muted mb-3">Join Our Community</h2> --}}
+                        <h3 class="display-3 px-lg-12 px-xxl-14">Únete a los cientos de profesionales contables que
+                            utilizan el Sistema Contable EICAP alrededor de toda Bolivia.</h3>
+                    </div>
+                    <!-- /column -->
+                </div>
+                <!-- /.row -->
+                <!-- /.row -->
+                <figure><img src="assets/img/photos/about26.png" srcset="./assets/img/photos/about26@2x.png 2x"
+                        alt="" /></figure>
+            </div>
+            <!-- /.container -->
+        </section>
+        <!-- /section -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="bg-dark text-inverse">
+        <div class="container py-13 py-md-15">
+            <div class="row gy-6 gy-lg-0">
+                <div class="col-md-4 col-lg-4">
+                    <div class="widget">
+                        {{-- <img class="mb-4" src="assets/img/logo-light.png"
+                            srcset="./assets/img/logo-light@2x.png 2x" alt="" /> --}}
+                        <p class="mb-4">© 2021 Sistema Contable EICAP. <br class="d-none d-lg-block" />All
+                            rights reserved.
+                        </p>
+                        <nav class="nav social social-white">
+                            <a href="https://www.facebook.com/escueladecapacitacioneicap"><i
+                                    class="uil uil-facebook-f"></i></a>
+                            <a href="https://www.youtube.com/channel/UCnH46JmDRItS0oXgfFtHzuw/videos"><i
+                                    class="uil uil-youtube"></i></a>
+                        </nav>
+                        <!-- /.social -->
+                    </div>
+                    <!-- /.widget -->
+                </div>
+                <!-- /column -->
+                <div class="col-md-12 col-lg-8">
+                    <div class="widget">
+                        <h4 class="widget-title text-white mb-3">Nuestra dirección.</h4>
+                        <address class="pe-xl-15 pe-xxl-17">
+                            Av. Santos Dumont entre 2do y 3er anillo, Frente al Colegio Militar de Aviación COMILAV,
+                            calle C, No. 15 Santa Cruz de la Sierra, Bolivia
+                        </address>
+                        Email: <a>escueladecapacitacion@eicap.com.bo</span></a><br />
+                        Cel: 76855562
+                        <!-- /.newsletter-wrapper -->
+                    </div>
+                    <!-- /.widget -->
+                </div>
+                <!-- /column -->
+            </div>
+            <!--/.row -->
+        </div>
+        <!-- /.container -->
+    </footer>
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous"></script>
+    <script src="assets/js/plugins.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
